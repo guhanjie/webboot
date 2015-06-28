ee082ae9a0a0a6ab71214c1597d372b1688736d9
顾汉杰 <guhj-a@grandsoft.com.cn>
2015-06-28 19:31:27 +0800

    在Controller层提供一个返回json数据的RESTful接口

--- src/main/java/com/guhanjie/controller/HomeController.java
+++ src/main/java/com/guhanjie/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.guhanjie.model.User;
 import com.guhanjie.service.UserService;
@@ -22,5 +23,18 @@ public class HomeController {
 		User user = userService.getUser(id);
 		model.addAttribute("user", user);
 		return "user";
+	}
+	
+	/**
+	 * 这里就是定义RESTful接口的地方，本系统可以将自己的服务接口暴露给外部系统
+	 * @param id
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="/rest/user/{userId}/", method=RequestMethod.GET)
+	@ResponseBody
+	public User restful(@PathVariable("userId") Integer id, Model model) {
+		User user = userService.getUser(id);
+		return user;
 	}
 }
